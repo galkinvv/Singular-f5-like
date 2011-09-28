e93f252e392e12f969ee935069ebb530c07834f9
Martin Lee <martinlee84@web.de>
2011-09-28 11:55:08 +0200

    cnfigure.in's and conversion for Singular and flint

---
+++ factory/FLINTconvert.h
@@ -0,0 +1,34 @@
+#ifndef FLINT_CONVERT_H
+#define FLINT_CONVERT_H
+
+#include <config.h>
+
+#ifdef HAVE_FLINT
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <fmpz.h>
+#include <fmpq.h>
+#include <fmpz_poly.h>
+#include <nmod_poly.h>
+#include <fmpq_poly.h>
+#ifdef __cplusplus
+}
+#endif
+
+#include "canonicalform.h"
+
+void convertCF2Fmpz (fmpz_t result, const CanonicalForm& f);
+void convertFacCF2Fmpz_poly_t (fmpz_poly_t result, const CanonicalForm& f);
+CanonicalForm convertFmpz2CF (fmpz_t coefficient);
+CanonicalForm convertFmpz_poly_t2FacCF (fmpz_poly_t poly, const Variable& x);
+void convertFacCF2nmod_poly_t (nmod_poly_t result, const CanonicalForm& f);
+CanonicalForm convertnmod_poly_t2FacCF (nmod_poly_t poly, const Variable& x);
+void convertCF2Fmpq (fmpq_t result, const CanonicalForm& f);
+CanonicalForm convertFmpq_t2CF (const fmpq_t q);
+CanonicalForm convertFmpq_poly_t2FacCF (fmpq_poly_t p, const Variable& x);
+void convertFacCF2Fmpq_poly_t (fmpq_poly_t result, const CanonicalForm& f);
+
+#endif
+#endif
