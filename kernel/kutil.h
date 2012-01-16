ae57aa0bf9d60fb4b38fb92678dab5e7ad93db92
Christian Eder <ederc@mathematik.uni-kl.de>
2012-01-16 21:50:54 +0100

    fixes some problems, keeps others: 1. redsig takes care of all reduction steps again 2. in the real reduction procedure we decide with    strat->currIdx if a sig-safety check is needed    or not => easier code, but it still does not work correctly!

--- kernel/kutil.h
+++ kernel/kutil.h
@@ -614,6 +614,7 @@ int ksReducePoly(LObject* PR,
 //            bound of currRing
 int ksReducePolySig(LObject* PR,
                  TObject* PW,
+                 long idx,
                  poly spNoether = NULL,
                  number *coef = NULL,
                  kStrategy strat = NULL);
